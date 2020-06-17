035a53ea18c77d2ff605239e34e385f42a382a3d
Jaiman <jaiman.patel@theonetechnologies.co.in>
2020-06-17 11:14:34 +0530

    Updates

---
+++ precache-manifest.c9a0ac0bbfe900ec182f762428d3abd5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84419ce988901d8ea3c23837db89d7de",
+    "url": "/JaimanpatelReact/index.html"
+  },
+  {
+    "revision": "833daf647c61108be0d1",
+    "url": "/JaimanpatelReact/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "36f037ca2a3c7fd8e117",
+    "url": "/JaimanpatelReact/static/js/2.b072d574.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/JaimanpatelReact/static/js/2.b072d574.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "833daf647c61108be0d1",
+    "url": "/JaimanpatelReact/static/js/main.abed5052.chunk.js"
+  },
+  {
+    "revision": "5f298d88ca79df38457b",
+    "url": "/JaimanpatelReact/static/js/runtime-main.e94ec0da.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/JaimanpatelReact/static/media/logo.5d5d9eef.svg"
+  }
+]);
